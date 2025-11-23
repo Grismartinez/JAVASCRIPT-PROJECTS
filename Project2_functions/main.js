ac53c6fdd8c5327e11bfc96fda40f89825d87088
Grismartinez <gris.martinez78@gmail.com>
2025-11-22 20:40:11 -0700

    Add files via upload

---
+++ Project2_functions/main.js
@@ -0,0 +1,12 @@
+// Create the variables
+let Sent1 = "This is the beginning of the string";
+let Sent2 = " and this is the end of the string";
+
+// Concatenate using the += operator
+Sent1 += Sent2;  // Sent1 now contains both strings
+
+// Write an expression using alert
+alert(Sent1);
+
+// Optional: Also log to the console
+console.log(Sent1);
