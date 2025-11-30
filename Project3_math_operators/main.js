49db75ee7489d7da1ba7057ed71233fafe9191d1
Grismartinez <gris.martinez78@gmail.com>
2025-11-30 17:24:48 -0600

    Add files via upload

--- Project3_math_operators/main.js
+++ Project3_math_operators/main.js
@@ -12,3 +12,28 @@ function subtractNumbers() {
     document.getElementById("result").textContent = 
         "The result of " + num1 + " - " + num2 + " is: " + difference;
 }
+
+function subtractions_Fuction() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+}
+
+function multiplications_Fuction() {
+    var multiplication = 5 * 2;
+    document.getElementById("Math1").innerHTML = "5 * 2 = " + multiplication;
+}
+      function increment() {
+        var X = 5;
+        X++;
+        document.getElementById("Math2").innerHTML = "5 incremented by 1 is: " + X;
+    }
+    function decrement() {
+        var Y = 5.25;
+        Y--;
+        document.getElementById("Math3").innerHTML = "5.25 decremented by 1 is: " + Y;
+    }
+  
+    function random_number() {
+        var R = Math.random() * 100;
+        document.getElementById("Math4").innerHTML = "A random number between 0 and 100: " + R;
+    }
