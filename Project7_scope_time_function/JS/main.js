e58876e37c2bb068d820d4b3b26aa491bc22101a
Grismartinez <gris.martinez78@gmail.com>
2025-12-01 23:15:17 -0600

    Add files via upload

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,35 @@
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+}   
+function Add_numbers_2() {
+    document.write(X + 100);
+}  
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }  
+} 
+function Age_Function() {
+    Age = document.getElementById("age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    } else {
+        Vote = "You are not old enough to vote.";
+    }
+    document.getElementById("How_old_are_you").innerHTML = Vote;
+}
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }   
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
