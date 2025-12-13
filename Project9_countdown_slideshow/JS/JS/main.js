e4818d7728e2f477f12148f4aa2c344a58a3a101
Grismartinez <gris.martinez78@gmail.com>
2025-12-13 15:32:48 -0700

    Add files via upload

--- Project9_countdown_slideshow/JS/JS/main.js
+++ Project9_countdown_slideshow/JS/JS/main.js
@@ -12,4 +12,19 @@ function countdown() {
         }
     }
     tick();
+    var images = [
+        "https://www.example.com/image1.jpg",
+        "https://www.example.com/image2.jpg",
+        "https://www.example.com/image3.jpg"
+    ];
+    var index = 0;  
+    function changeImage() {
+        document.getElementById("slideshow").src = images[index];
+        index++;
+        if (index >= images.length) {
+            index = 0;
+        }
+        setTimeout(changeImage, 2000);
+    }
+    changeImage();
 }
