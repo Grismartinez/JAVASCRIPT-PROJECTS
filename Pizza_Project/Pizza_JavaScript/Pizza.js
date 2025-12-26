6c62e59b8f60bda43bdff9868fa2f58a239fb2b7
Grismartinez <gris.martinez78@gmail.com>
2025-12-26 16:40:57 -0700

    Add files via upload

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -4,13 +4,15 @@ function getReceipt() {
     var text1 = "<h3>You Ordered:</h3>";    
     var runningTotal = 0;
     var sizeTotal = 0;
-    var sizeArray = document.getElementsByClassName("size");    
+    var sizeArray = document.getElementsByClassName("size");
+    //Loop through pizza size to find selected size    
     for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
             text1 = text1 + selectedSize + "<br>";
         }
     }
+    //Determine pizza size cost
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
@@ -22,16 +24,22 @@ function getReceipt() {
     } else if (selectedSize === "Extra Large Pizza") {
         sizeTotal = 16;
     }
+    //Add size total to running total
     runningTotal = sizeTotal;
+    //Show size total and running total
     console.log(selectedSize + " = $" + sizeTotal + ".00");
     console.log("Subtotal: $" + runningTotal + ".00");
     console.log("size text1:" + text1);
+    //Call topping function
     getTopping(runningTotal, text1);
 }
+// Determine topping cost
 function getTopping(runningTotal, text1) {
+//This section handles toppings
     var toppingTotal = 0;
     var selectedTopping = [];
-    var toppingArray = document.getElementsByClassName("toppings"); 
+    var toppingArray = document.getElementsByClassName("toppings");
+    //Loop through toppings to see which were selected 
     for (var j = 0; j < toppingArray.length; j++) {
         if (toppingArray[j].checked) {
             selectedTopping.push(toppingArray[j].value);
@@ -39,17 +47,21 @@ function getTopping(runningTotal, text1) {
             text1 = text1 + toppingArray[j].value + "<br>";
         }
     }
+    //Determine how much the toppings cost
     var toppingCount = selectedTopping.length;
     if (toppingCount > 1) {
         toppingTotal = (toppingCount - 1);
     } else {
         toppingTotal = 0;
     }
+    //This section Adds topping total to running total
     runningTotal = (runningTotal + toppingTotal);
+    //Show topping total and running total
     console.log("Total selected topping items: " + toppingCount);
     console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
     console.log("Topping total is: $" + toppingTotal + ".00");
     console.log("Final Total: $" + runningTotal + ".00");
+    //Display the order summary
     document.getElementById("showText").innerHTML = text1;
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$" + runningTotal + ".00" + "</strong></h3>";
 }   
